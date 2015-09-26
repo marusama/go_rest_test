13c49b070a7dffbb0fb4ca84493d344af1ee42c4
Maru Sama <maru.sama@gmail.com>
2015-09-27 02:55:18 +0300

    1

--- main.go
+++ main.go
@@ -3,19 +3,44 @@ package main
 import (
 	"fmt"
 	"github.com/ant0ine/go-json-rest/rest"
+	"github.com/gocql/gocql"
 	"log"
 	"net/http"
 )
 
 // some options for API
 const (
+	CassandraCluster = "127.0.0.1"
+	Keyspace         = "test_rest"
+
 	ApiHost                  = ":8080"
 	AuthRealm                = "test_realm"
 	SessionDurationInMinutes = 60
 )
 
 func main() {
-	runApiServer()
+	cluster := gocql.NewCluster(CassandraCluster)
+	cluster.Keyspace = "demo"
+	session, _ := cluster.CreateSession()
+	defer session.Close()
+
+	ensureDbSchema(session)
+
+	//runApiServer()
+}
+
+func ensureDbSchema(session *gocql.Session) {
+	queryCountTables := "SELECT COUNT(*) FROM system.schema_columnfamilies WHERE keyspace_name='" + Keyspace + "';"
+	var tableCount int
+	session.Query(queryCountTables).Scan(&tableCount)
+	if tableCount == 0 {
+		queryCreateKeyspace := "CREATE KEYSPACE " + Keyspace + ";"
+		queryCreateTableUsers := "CREATE TABLE " + Keyspace + ".users (login text, password text, PRIMARY KEY (login));"
+		queryCreateTableUserAuths := "CREATE TABLE " + Keyspace + ".user_auths (token text, login text, exp_time timestamp, PRIMARY KEY (login));"
+		session.Query(queryCreateKeyspace).Exec()
+		session.Query(queryCreateTableUsers).Exec()
+		session.Query(queryCreateTableUserAuths).Exec()
+	}
 }
 
 func runApiServer() {
